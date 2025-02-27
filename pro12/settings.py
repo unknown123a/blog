b297055e60cde42f9e23c466b7da04eadad4de5b
unknown123a <ulogeshwaran4@gmail.com>
2025-02-27 15:36:18 +0530

    added locl host

--- pro12/settings.py
+++ pro12/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-hawkv4rip-vprk9lerpki*@5ux*#3ds-i=7kyt#p7h&6#7tmi7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-production-5107.up.railway.app','https://blog-production-5107.up.railway.app']
+ALLOWED_HOSTS = ['blog-production-5107.up.railway.app','https://blog-production-5107.up.railway.app','localhost:8000']
 CSRF_TRUSTED_ORIGINS = ['https://blog-production-5107.up.railway.app']
 
 
