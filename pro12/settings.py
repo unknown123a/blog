ec32d2233e80924dadd413a980366a91c5c21c49
unknown123a <ulogeshwaran4@gmail.com>
2025-03-01 14:50:57 +0530

    updated static file problem

--- pro12/settings.py
+++ pro12/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'app1'/'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
