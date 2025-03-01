fdbb6d8494c501a61ce9c90f727c502b298c94dd
unknown123a <ulogeshwaran4@gmail.com>
2025-03-01 18:11:47 +0530

    message

--- pro12/settings.py
+++ pro12/settings.py
@@ -38,7 +38,8 @@ DEBUG = True
 # ALLOWED_HOSTS = ['blog-production-5107.up.railway.app','https://blog-production-5107.up.railway.app']
 # CSRF_TRUSTED_ORIGINS = ['https://blog-production-5107.up.railway.app']
 
-ALLOWED_HOSTS = ["blog-production-5107.up.railway.app"]
+# ALLOWED_HOSTS = ["blog-production-5107.up.railway.app"]
+ALLOWED_HOSTS = ["blog-production-5107.up.railway.app", "127.0.0.1", "localhost"]
 
 CSRF_TRUSTED_ORIGINS = ['https://blog-production-5107.up.railway.app']
 
